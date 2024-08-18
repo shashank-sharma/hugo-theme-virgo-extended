1b8e5242ca89ecd7c2f3f5315ac014c83ffe49d3
Shashank Sharma <shashank.sharma98@gmail.com>
2024-08-19 02:13:35 +0530

    [API]: Device feature added

---
+++ assets/js/init/home.js
@@ -0,0 +1,92 @@
+export default function initDeviceStatus(apiEndpoint) {
+    const deviceTemplate = `
+      <div class="holo-badge">
+        <div class="holo-content">
+          <div class="holo-header">
+            <div class="holo-icon-test">
+            </div>
+            <div class="holo-title"></div>
+          </div>
+          <div class="holo-body">
+            <p class="holo-sync"></p>
+            <div class="holo-status">
+              <span class="holo-status-dot"></span>
+              <span class="holo-status-text"></span>
+            </div>
+            <p class="holo-current-app"></p>
+          </div>
+        </div>
+        <div class="holo-glow"></div>
+      </div>
+    `;
+  
+    function createDeviceElement() {
+      const template = document.createElement('template');
+      template.innerHTML = deviceTemplate.trim();
+      return template.content.firstChild;
+    }
+  
+    function updateDeviceStatus(device, element) {
+      element.querySelector('.holo-title').textContent = device.name;
+      const syncElement = element.querySelector('.holo-sync');
+      const statusDot = element.querySelector('.holo-status-dot');
+      const statusText = element.querySelector('.holo-status-text');
+      const currentAppText = element.querySelector('.holo-current-app');
+      
+      if (device.is_public) {
+        element.classList.remove('not-available');
+        syncElement.textContent = `Last Sync: ${formatDate(device.last_sync)}`;
+        statusDot.className = `holo-status-dot ${device.is_online ? 'online' : 'offline'}`;
+        statusText.textContent = device.is_online ? 'Online' : 'Offline';
+        currentAppText.textContent = device.current_app ? device.current_app : '';
+      } else {
+        element.classList.add('not-available');
+        syncElement.textContent = 'Not Available';
+        statusDot.className = 'holo-status-dot not-available';
+        statusText.textContent = 'Private';
+        currentAppText.textContent = 'Private';
+      }
+    }
+  
+    function formatDate(dateString) {
+      if (!dateString) return 'Never';
+      return new Date(dateString).toLocaleString();
+    }
+  
+    function fetchAndUpdateDevices() {
+        fetch(apiEndpoint)
+          .then(response => response.json())
+          .then(data => {
+            const container = document.getElementById('device-container');
+            container.innerHTML = '';
+            const sortedDevices = data.devices.sort((a, b) => {
+
+              const getDateValue = (device) => {
+                return device.last_sync ? new Date(device.last_sync).getTime() : 0;
+              };
+      
+              if (a.is_public && !b.is_public) return -1;
+              if (!a.is_public && b.is_public) return 1;
+              if (a.is_public && b.is_public) {
+                if (a.is_online && !b.is_online) return -1;
+                if (!a.is_online && b.is_online) return 1;
+                
+                return getDateValue(b) - getDateValue(a);
+              }
+      
+              return getDateValue(b) - getDateValue(a);
+            });
+      
+            sortedDevices.forEach(device => {
+              const deviceElement = createDeviceElement();
+              updateDeviceStatus(device, deviceElement);
+              container.appendChild(deviceElement);
+            });
+          })
+          .catch(error => console.error('Error fetching device status:', error));
+      }
+  
+    fetchAndUpdateDevices();
+    const intervalId = setInterval(fetchAndUpdateDevices, 5 * 60 * 1000);
+    return () => clearInterval(intervalId);
+  }
--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,16 @@
 import greet from "js/init/greet";
 import initEventBinding from "js/init/eventBinding";
+import home from "js/init/home";
 import initImage from "js/init/initImage";
 import initCodeBlock from "js/init/initCodeBlock";
 import enhanceOrgMode from './js/init/enhanceOrgMode';
 import enhanceMarkdown from './js/init/enhanceMarkdown';
 import runMisc from './js/init/runMisc';
+import * as params from '@params';
 
+if (params.params.backend && params.params.backend.deviceapiendpoint) {
+    home(params.params.backend.deviceapiendpoint)
+}
 greet();
 enhanceOrgMode();
 initCodeBlock();
